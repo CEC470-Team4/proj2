517aa1c16a7b64941164c23bcdd86dfeec628ea1
M. Haruyama <harzapm@gmail.com>
2023-11-16 17:56:23 -0500

    Update cec470_team4_project2.c

--- cec470_team4_project2.c
+++ cec470_team4_project2.c
@@ -1,7 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(void)
+#define HALT_OPCODE 0x19
+
+void fetchNextInstruction(void);
+void executeInstruction(void);
+
+unsigned char memory[65536];
+unsigned char ACC = 0;
+unsigned char IC = 0;
+unsigned int MAR = 0;
+unsigned int PC = 0;
+
+int main(int argc, char * argv[])
 {
+
+    // execution loop
+    // continue fetching and executing
+    // until PC points to a HALT instrction
+    while (memory[PC] != HALT_OPCODE)
+    {
+        fetchNextInstruction();
+        executeInstruction;
+    }
+
     return 0;
 }
