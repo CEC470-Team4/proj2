b2fbb4bb1e8699dd1691f05a8eaa4ab128b88c4f
M. Haruyama <harzapm@gmail.com>
2023-12-02 16:50:19 -0500

    IT WORKS

--- cec470_team4_project2.c
+++ cec470_team4_project2.c
@@ -10,25 +10,28 @@
 #include <string.h>
 #include <stdbool.h>
 
-// Special Opcode Masks
-#define HALT_OPCODE 0x19
-#define NOP_OPCODE 0x18
+// Special
+#define HALT_OPCODE 0x19 // 0b0001_1001
+#define NOP_OPCODE 0x18 // 0b0001_1000
 
-// Math Opcode Masks
-#define MATH_OPCODE 0x80
-#define MATH_FUNC 0x70
-#define MATH_SRC 0x03
-#define MATH_DST 0x0C
+// Math
+#define MATH_MASK 0x80 // 0b1000_0000
+#define MATH_OPCODE 0x01 // 0b0001
+#define MATH_FUNC_MASK 0x70 // 0b0111_0000
+#define MATH_DST_MASK 0x0C // 0b01100
+#define MATH_SRC_MASK 0x03 // 0b0011
 
-// Memory Opcode Masks
-#define MEM_OPCODE 0x00
-#define MEM_FUNC 0x08
-#define MEM_REG 0x04
-#define MEM_METH 0x03
+// Memory
+#define MEM_MASK 0xF0 // 0b1111_0000
+#define MEM_OPCODE 0x0000 // 0b0000
+#define MEM_FUNC_MASK 0x08 // 0b1000_0000
+#define MEM_REG_MASK 0x04 // 0b0100
+#define MEM_METH_MASK 0x03 // 0b0011
 
-// Branch Opcode Masks
-#define BRANCH_OPCODE 0x10
-#define BRANCH_TYPE 0x07
+// Branch
+#define BRANCH_MASK 0xF8 // 0b1111_1000
+#define BRANCH_OPCODE 0x02 // 0b0_0010
+#define BRANCH_TYPE_MASK 0x07 // 0b0111
 
 FILE *fileptr;
 
@@ -46,8 +49,8 @@ void executeInstruction(void);
 unsigned int address(void);
 
 void mathOp(void);
-unsigned int mathOpSrc(void);
 unsigned int * mathOpDst(void);
+unsigned int mathOpSrc(void);
 
 void memOp(void);
 unsigned int memOpMeth(void);
@@ -68,6 +71,7 @@ int main(int argc, char * argv[])
         fetchNextInstruction();
         executeInstruction();
     }
+    printf("end loop\n");
 
     return 0;
 }
@@ -97,46 +101,59 @@ void readMemory(void) // Maegan
     fclose(fileptr);
 }
 
-void fetchNextInstruction(void) // Maegan
+void fetchNextInstruction(void) // Maegan & Milan
 {
     int bytes = 0;
     IR = memory[PC];
 
     // Math Operation
-    if ((IR & MATH_OPCODE) == MATH_OPCODE) {
-        if ((IR & MATH_FUNC) >> 0x40) {
-            if ((IR & MATH_DST) == MATH_DST) {
-
+    if ((IR & MATH_MASK) >> 7 == MATH_OPCODE) {
+        
+        // FUNC is INC, DEC, NOT
+        if ( (IR & MATH_FUNC_MASK) >> 4 > 0x04)
+        {
+            //DST is a memory address
+            if ((IR & MATH_DST_MASK) >> 2 == 0x03)
                 bytes = 2;
-            } else {
+            else
                 bytes = 1;
-            }
-        } else if (((IR & MATH_DST) != MATH_DST) && ((IR & 0x02) == 0)) {
-            bytes = 1;
-        } else if ((IR & 0x0b) == 2) {
-            bytes = 2;
-        } else {
-            bytes = 3;
         }
+
+        //SRC is *MAR or ACC AND DST is not a memory address
+        else if ( ((IR & MATH_SRC_MASK) >> 1 == 0x00) && ((IR & MATH_DST_MASK) >> 2 != 0x03))
+            bytes = 1;
+
+        //DST is *MAR or ACC AND SRC is constant
+        else if ( ((IR & MATH_DST_MASK) >> 3 == 0x00) && ( (IR & MATH_SRC_MASK) == 0x02))
+            bytes = 2;
+
+        //destination OR source are an address
+        else
+            bytes = 3;
+    }
 
     // Memory Operation
-    } else if ((IR & MEM_OPCODE) == MEM_OPCODE) {
-        if ((IR & MEM_METH) == 2) {
+    else if ((IR & MEM_MASK) >> 4 == MEM_OPCODE) 
+    {
+        //METH is *MAR
+        if ((IR & MEM_METH_MASK) == 0x02) 
             bytes = 1;
-        } else if (!((IR & MEM_REG) >> 2) && ((IR & MEM_METH) == 1)){
+
+        //REG is ACC AND METH is constant
+        else if (((IR & MEM_REG_MASK) >> 2 == 0x00) && ((IR & MEM_METH_MASK) == 0x01))
             bytes = 2;
-        } else {
+        else
             bytes = 3;
-        }
+
+    } 
 
     // Branch Operation
-    } else if ((IR & BRANCH_OPCODE) == BRANCH_OPCODE) {
+    else if ((IR & BRANCH_MASK) >> 3 == BRANCH_OPCODE)
         bytes = 3;
 
     // Special/Illegal Operations
-    } else {
+    else
         bytes = 1;
-    }
 
     PC += bytes;
 }
@@ -146,15 +163,15 @@ void executeInstruction(void) // Milan and Tabitha
     // check for regular opcodes first //
 
     // Mathematical operations
-    if ((IR & MATH_OPCODE) == MATH_OPCODE)
+    if ((IR & MATH_MASK) >> 7 == MATH_OPCODE)
         mathOp();
 
     // Memory operations
-    else if ((IR & MEM_OPCODE) == MEM_OPCODE)
+    else if ((IR & MEM_MASK) >> 4 == MEM_OPCODE)
         memOp();
 
     // Branches/Jumps
-    else if ((IR & BRANCH_OPCODE) == BRANCH_OPCODE)
+    else if ((IR & BRANCH_MASK) >> 3 == BRANCH_OPCODE)
         branch();
 
     // check for HALT, NOP, or illegal opcodes last //
@@ -182,17 +199,17 @@ void executeInstruction(void) // Milan and Tabitha
 
 unsigned int address()
 {
-    // retrieves the two byte address proceeding the opcode
-    // also converts the address from big-endian to little-endian
+    // retrieves two byte address proceeding the opcode
+    // also converts address from big-endian to little-endian
     return (memory[PC - 2] << 8) + memory[PC - 1];
 }
 
 void mathOp()
 {
-    unsigned int src = mathOpSrc();
     unsigned int * dst = mathOpDst();
+    unsigned int src = mathOpSrc();
 
-    switch((IR & MATH_FUNC) >> 4)
+    switch( (IR & MATH_FUNC_MASK) >> 4)
     {
         // 0b_000 - AND
         case 0: 
@@ -237,61 +254,28 @@ void mathOp()
 
 }
 
-unsigned int mathOpSrc() // Milan
-{
-    unsigned int src = 0;
-    switch ((IR & MATH_SRC))
-    {
-        //Indirect (MAR used as a pointer)
-        case 0:
-            src = memory[MAR];
-            break;
-
-        //Accumulator ACC
-        case 1:
-            src = ACC;
-            break;
-
-        //Constant
-        case 2: 
-            //if destination is MAR or Address
-            if((IR & 0x08) == 0x08)
-                src = address();
-            else
-                src = memory[PC - 1];
-            break;
-
-        //Memory
-        case 3: 
-            src = memory[address()];
-            break;
-    }
-
-    return src;
-}
-
 unsigned int * mathOpDst() // Milan
 {
     // returns the memory address of the destination
     unsigned int *dst = NULL;
-    switch ((IR & MATH_DST) >> 2)
+    switch ((IR & MATH_DST_MASK) >> 2)
     {
+        // 0b00 - Indirect (MAR used as a pointer)
         case 0:
-        //Indirect (MAR used as a pointer)
-        dst = (unsigned int *) &memory[MAR];
-        break;
+            dst = (unsigned int *) &memory[MAR];
+            break;
         
+        // 0b01 - Accumlator ACC
         case 1:
-        //Accumlator ACC
-        dst = (unsigned int *) &ACC;
-        break;
+            dst = (unsigned int *) &ACC;
+            break;
 
-        //Address register MAR
+        // 0b10 - Address Register MAR
         case 2: 
             dst = &MAR;
             break;
 
-        //Memory
+        // 0b11 - Memory Address
         case 3:
             dst = (unsigned int *) &memory[address()];
             break;
@@ -300,15 +284,46 @@ unsigned int * mathOpDst() // Milan
     return dst;
 }
 
+unsigned int mathOpSrc() // Milan
+{
+    unsigned int src = 0;
+    switch (IR & MATH_SRC_MASK)
+    {
+        // 0b00 - Indirect (MAR used as a pointer)
+        case 0:
+            src = memory[MAR];
+            break;
 
+        // 0b01 - Accumulator ACC
+        case 1:
+            src = ACC;
+            break;
+
+        // 0b10 - Constant
+        case 2: 
+            //if destination is MAR or Address
+            if(IR & 0x08 == 0x08)
+                src = address();
+            else
+                src = memory[PC - 1];
+            break;
+
+        // 0b11 - Memory
+        case 3: 
+            src = memory[address()];
+            break;
+    }
+
+    return src;
+}
 
 void memOp()
 {
     // 1 - LOAD
-    if( (IR & MEM_FUNC) >> 3) 
+    if( (IR & MEM_FUNC_MASK) >> 3) 
     {
         // LOAD MAR
-        if ((IR & MEM_REG) >> 2)
+        if ((IR & MEM_REG_MASK) >> 2)
             MAR = (memory[memOpMeth()] << 8) + memory[memOpMeth() + 1];
 
         // LOAD ACC
@@ -320,7 +335,7 @@ void memOp()
     else 
     {
         // STOR MAR
-        if ((IR & MEM_REG) >> 2)
+        if ((IR & MEM_REG_MASK) >> 2)
         {
             memory[memOpMeth()] = (MAR & 0xFF00) >> 8;
             memory[memOpMeth() + 1] = (MAR & 0x00FF);
@@ -336,7 +351,7 @@ void memOp()
 unsigned int memOpMeth()
 {
     unsigned int opAddress = 0;
-    switch (IR & MEM_METH)
+    switch (IR & MEM_METH_MASK)
     {
         // 0b_000
         case 0: 
@@ -348,7 +363,7 @@ unsigned int memOpMeth()
         case 1: 
         //Operand is used as constant
             // if MAR
-            if((IR & MEM_REG) >> 2)
+            if((IR & MEM_REG_MASK) >> 2)
                 opAddress = PC - 2; //constant is 2 byte
             // if ACC
             else
@@ -365,10 +380,10 @@ unsigned int memOpMeth()
     return opAddress;
 }
 
-void branch () //Milan and Tabitha
+void branch() //Milan and Tabitha
 {
     bool brch = false;
-    switch(IR & BRANCH_TYPE)
+    switch(IR & BRANCH_TYPE_MASK)
     {
         case 0: // 0b000 - BRA
             brch = true;
